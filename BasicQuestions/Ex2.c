b0a382372767a486a57575cbd7ba263db4ad7dc2
SouthPortela <marcosmarquesportela1@gmail.com>
2025-12-18 13:43:23 -0300

    Questão 2, manipulação de pilha

--- BasicQuestions/Ex2.c
+++ BasicQuestions/Ex2.c
@@ -25,3 +25,37 @@ void acessar(PilhaHistorico *p, char *site){
   printf("Acessou: %s (Valor do topo: %d)\n", site, p->topo);
 }
 
+void voltar(PilhaHistorico *p){
+  if (p->topo == -1){
+    printf("ERRO: Ja esta no inicio, nao tem destino no voltar.\n");
+    return;
+  }
+  p->topo--;
+  if (p->topo == -1){
+    printf("Voltou para o inicio.\n");
+  }else{
+    printf("Voltou para: %s\n", p->urls[p->topo]);
+  }
+}
+
+int main(){
+  PilhaHistorico browser;
+  inicializar(&browser);
+
+  printf("---- NAVEGANDO ----\n");
+  acessar(&browser, "www.youtube.com");
+  acessar(&browser, "www.uol.com.br");
+  acessar(&browser, "www.instagram.com");
+  acessar(&browser, "www.github.com");
+
+  printf("\n----- VOLTAR -----\n");
+
+  voltar(&browser);
+  voltar(&browser);
+  voltar(&browser);
+  voltar(&browser);
+
+  return 0;
+}
+
+
